772a26082b454813cee64800f4532e93f458f9dc
zhuhaoran <zhuhaoran@ambition.com>
2022-08-04 00:49:51 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入去全局的样式
+import '@/assets/css/index.less'
 
 Vue.config.productionTip = false
 
